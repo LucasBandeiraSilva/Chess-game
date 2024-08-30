114dd42cbab7893d0f0954739befd44e208dc194
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-08-30 10:57:03 -0300

    feat: Created Position class

--- src/application/App.java
+++ src/application/App.java
@@ -1,6 +1,10 @@
 package application;
+
+import boardgame.Position;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args)  {
+        Position position = new Position(12, 12);
+        System.out.println(position);
     }
 }
