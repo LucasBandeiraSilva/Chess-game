be90af7412dfffdb7a54196c8f049eeb753b47d0
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-09-06 10:18:47 -0300

    Refactor: move toString method to the end of the class

--- src/chess/Pieces/Rook.java
+++ src/chess/Pieces/Rook.java
@@ -12,54 +12,55 @@ public class Rook extends ChessPiece {
     }
 
     @Override
-    public String toString() {
-        return "R";
-    }
-    
-    @Override
     public boolean[][] possibleMoves() {
         boolean[][] matrix = new boolean[getBoard().getRows()][getBoard().getColumns()];
-        Position pos = new Position(0,0);
-        //above
-        pos.setValues(position.getRow()-1, position.getColumn());
+        Position pos = new Position(0, 0);
+        // above
+        pos.setValues(position.getRow() - 1, position.getColumn());
         while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
             matrix[pos.getRow()][pos.getColumn()] = true;
-            pos.setRow((pos.getRow()-1));
-        }
-        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
-            matrix[pos.getRow()][pos.getColumn()] = true;
-            
-        }
-        //left
-        pos.setValues(position.getRow(), position.getColumn()-1);
-        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
-            matrix[pos.getRow()][pos.getColumn()] = true;
-            pos.setColumn((pos.getColumn()-1));
+            pos.setRow((pos.getRow() - 1));
         }
         if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
             matrix[pos.getRow()][pos.getColumn()] = true;
 
         }
-        //right
-        pos.setValues(position.getRow(), position.getColumn()+1);
+        // left
+        pos.setValues(position.getRow(), position.getColumn() - 1);
         while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
             matrix[pos.getRow()][pos.getColumn()] = true;
-            pos.setColumn(pos.getColumn()+1);
+            pos.setColumn((pos.getColumn() - 1));
         }
         if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
             matrix[pos.getRow()][pos.getColumn()] = true;
 
         }
-        //below
-        pos.setValues(position.getRow()+1, position.getColumn());
+        // right
+        pos.setValues(position.getRow(), position.getColumn() + 1);
         while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
             matrix[pos.getRow()][pos.getColumn()] = true;
-            pos.setRow((pos.getRow()+1));
+            pos.setColumn(pos.getColumn() + 1);
         }
         if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
             matrix[pos.getRow()][pos.getColumn()] = true;
-            
+
+        }
+        // below
+        pos.setValues(position.getRow() + 1, position.getColumn());
+        while (getBoard().positionExists(pos) && !getBoard().thereIsAPiece(pos)) {
+            matrix[pos.getRow()][pos.getColumn()] = true;
+            pos.setRow((pos.getRow() + 1));
+        }
+        if (getBoard().positionExists(pos) && isThereOpponentPiece(pos)) {
+            matrix[pos.getRow()][pos.getColumn()] = true;
+
         }
         return matrix;
     }
+
+    @Override
+    public String toString() {
+        return "R";
+    }
+
 }
