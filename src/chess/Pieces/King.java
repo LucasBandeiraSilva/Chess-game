00e6926400ab788352e9cbd00ead6fa3633abff1
Lucas Bandeira <lucasbandeirai40@gmail.com>
2024-09-06 10:38:07 -0300

    Refactor: move toString method to the end of the King class

--- src/chess/Pieces/King.java
+++ src/chess/Pieces/King.java
@@ -11,11 +11,6 @@ public class King extends ChessPiece {
         super(board, color);
     }
 
-    @Override
-    public String toString() {
-        return "K";
-    }
-
     private boolean canMove(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p == null || p.getColor() != getColor();
@@ -76,6 +71,11 @@ public class King extends ChessPiece {
         }
 
         return mat;
+    }
+
+    @Override
+    public String toString() {
+        return "K";
     }
 
 }
